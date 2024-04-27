24d53137b755d74487c8832b68f0c9e4f2445708
Lodovico Ardissao <lodoviko@gmail.com>
2024-04-27 15:37:51 -0300

    20240427 - Criação do projeto convencao-api

---
+++ src/main/java/com/lodoviko/convencao/ConvencaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.lodoviko.convencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvencaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvencaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lodoviko/convencao/ConvencaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodoviko.convencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvencaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
